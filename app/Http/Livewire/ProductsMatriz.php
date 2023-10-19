66de7a69f68c4a82c68a7df33c383f33df3fcdbf
Raique Varjao <raiquegomes55@gmail.com>
2023-10-19 11:28:24 -0300

    Update ProductsMatriz.php

--- app/Http/Livewire/ProductsMatriz.php
+++ app/Http/Livewire/ProductsMatriz.php
@@ -3,15 +3,10 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
-use App\Models\User;
 use App\Models\Filial;
-use App\Models\Products;
 
 class ProductsMatriz extends Component
 {
-    public $filial_all;
-
     public function render()
     {
         $this->filial_all = Filial::all();
